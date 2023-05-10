5ac3a1dfc581ccbdcff3f53e3b168b0b2d2b1202
Ilona Lameka <lamekailona@gmail.com>
2023-05-09 20:46:05 -0400

    Created test bench file for main decoder

---
+++ MainDec/tb_mainDec.sv
@@ -0,0 +1,22 @@
+//////////////////////////////////////////////////////////////////////////////////
+// The Cooper Union                                                             //
+// ECE 251 Spring 2023                                                          //
+// Engineers: Ridwan Hussain and Ilona Lameka                                   //
+//                                                                              //
+//     Create Date: 2023-05-09                                                  //
+//     Module Name: tb_mainDec                                                  //
+//     Description: Test bench for main decoder                                 //
+//                                                                              //
+// Revision: 1.0                                                                //
+//                                                                              //
+//////////////////////////////////////////////////////////////////////////////////
+`ifndef MAINDEC
+`define MAINDEC
+
+`timescale 1ns/100ps
+
+module tb_mainDec();
+
+endmodule // tb_mainDec
+
+`endif
