d91e1c6da11094b9ee07d53e74afbbdf0943f2d7
Ridwan Hussain <ridwanhussain2003@gmail.com>
2023-05-14 12:06:39 -0400

    Updating repo stuff. Not much to say.

--- Datapath/datapath.sv
+++ Datapath/datapath.sv
@@ -1,23 +1,24 @@
-//////////////////////////////////////////////////////////////////////////
-//                                                                      //
-// EXAMPLE module                                                       //
-//                                                                      //
-// An example module for your Computer Architecture Elements Catalog    //
-//                                                                      //
-// module: example                                                      //
-// hdl: Verilog                                                         //
-//                                                                      //
-// author: Ridwan Hussain <ridwan.hussain@cooper.edu>                   //
-//                                                                      //
-//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+// The Cooper Union                                                             //
+// ECE 251 Spring 2023                                                          //
+// Engineers: Ridwan Hussain and Ilona Lameka                                   //
+//                                                                              //
+//     Create Date: 2023-05-13                                                  //
+//     Module Name: datapath                                                     //
+//     Description: The datapath the integrates RegFile, Alu, dff, adder, sl2, mux2, signext                                                  //
+//                                                                              //
+// Revision: 1.0                                                                //
+//                                                                              //
+//////////////////////////////////////////////////////////////////////////////////
 
 `ifndef EXAMPLE
 `define EXAMPLE
 
-module example(A, B, C);
+module example
+	#(parameter n = 32, parameter r = 6)
 	// ---- PORT DEFINITIONS ---- //
-	input  [3:0] A, B;
-	output [3:0] C;
+	(input  [3:0] A, B,
+	output [3:0] C);
 
 	// ---- MODULE DESIGN IMPLEMENTATION ---- //
 
