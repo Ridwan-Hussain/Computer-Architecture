bfdd82f1bb6c2710eafc1ca95f7891a6660197b4
Ridwan Hussain <ridwanhussain2003@gmail.com>
2023-05-14 19:35:38 -0400

    Added Sl5 back with structural code.

---
+++ Sl5/sl5.sv
@@ -0,0 +1,38 @@
+//////////////////////////////////////////////////////////////////////////////////
+// The Cooper Union                                                             //
+// ECE 251 Spring 2023                                                          //
+// Engineers: Ridwan Hussain and Ilona Lameka                                   //
+//                                                                              //
+//     Create Date: 2023-05-13                                                  //
+//     Module Name: sl5                                                         //
+//     Description: Shift 5 bits to the left (the same as mulitply by 32). We   //
+//     implemented so that the 5 MSB will get dropped instead of rolling over.  //
+//                                                                              //
+// Revision: 1.0                                                                //
+//                                                                              //
+//////////////////////////////////////////////////////////////////////////////////
+
+`ifndef sl5
+`define sl5
+
+module sl5
+	#(parameter n = 32)
+	// ---- PORT DEFINITIONS ---- //
+	(input [(n-1):0] num,
+	output [(n-1):0] num32);
+
+	// ---- MODULE DESIGN IMPLEMENTATION ---- //
+	genvar i;
+	generate for (i = 0; i < (n - 5); i = i + 1) begin
+		or(num32[i+5], num[i], 0);
+	end
+	endgenerate
+	and(num32[0], 0, 0);
+	and(num32[1], 0, 0);
+	and(num32[2], 0, 0);
+	and(num32[3], 0, 0);
+	and(num32[4], 0, 0);
+
+endmodule
+
+`endif // sl5
---
+++ Sl5/tb_sl5.sv
@@ -0,0 +1,49 @@
+//////////////////////////////////////////////////////////////////////////////////
+// The Cooper Union                                                             //
+// ECE 251 Spring 2023                                                          //
+// Engineers: Ridwan Hussain and Ilona Lameka                                   //
+//                                                                              //
+//     Create Date: 2023-05-13                                                  //
+//     Module Name: tb_sl5                                                      //
+//     Description: Testbench for sl5. Makes sure the bits are shifted to left  //
+//     left by 5 and overflow is handled for the 5 MSB get dropped.             //
+//                                                                              //
+// Revision: 1.0                                                                //
+//                                                                              //
+//////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns/100ps
+`include "./sl5.sv"
+
+module tb_sl5 
+  // ---- Defining variables to be used ---- //
+	#(parameter n = 32);
+	reg [(n-1):0] num;   //inputs are reg for test bench
+	wire [(n-1):0] num32;     //outputs are wire for test bench
+   
+	// ---- INSTANTIATE UNIT UNDER TEST (UUT) ---- //
+	sl5 uut(.num(num), .num32(num32));
+	
+	// ---- INITIALIZE TEST BENCH ---- //
+	initial begin
+		$dumpfile("sl5.vcd"); // for Makefile, make dump file same as module name
+		$dumpvars(0, uut);
+		$monitor("num=%b=%d, num32=%b=%d", num, num, num32, num32);
+	end
+
+	//apply input vectors
+	initial begin: apply_stimulus
+		#1 num = 32'h00000000;
+		#1 num = 32'h00000001;
+		#1 num = 32'h00001111;
+		#1 num = 32'h0000BEEF;
+		#1 num = 32'h11111111;
+		#1 num = 32'h13579BDF;
+		#1 num = 32'hF0000000;
+		#1 num = 32'hFFFFFFFF;
+		$finish;
+	end
+
+endmodule
+
+// `endif // tb_sl5
