522f7da5cb9c460f4519ebc267419d3494f1823f
Ridwan Hussain <ridwanhussain2003@gmail.com>
2023-05-14 19:01:21 -0400

    Revert "Added Sl2 to Sl5 since memory is word addressable, not byte addressable."
    This reverts commit e0a67d2be1385cb0285e59b71dafc24cc42eed43.

--- Sl2/tb_sl2.sv
+++ Sl2/tb_sl2.sv
@@ -4,39 +4,37 @@
 // Engineers: Ridwan Hussain and Ilona Lameka                                   //
 //                                                                              //
 //     Create Date: 2023-05-13                                                  //
-//     Module Name: tb_sl5                                                      //
-//     Description: Testbench for sl5. Makes sure the bits are shifted to the   //
-//     left by 5 and overflow is handled so the 5 MSB get dropped.              //
+//     Module Name: tb_sl2                                                      //
+//     Description: Testbench for sl2. Makes sure the bits are shifted to the   //
+//     left by 2 and overflow is handled so the 2 MSB get dropped.              //
 //                                                                              //
 // Revision: 1.0                                                                //
 //                                                                              //
 //////////////////////////////////////////////////////////////////////////////////
 
 `timescale 1ns/100ps
-`include "./sl5.sv"
+`include "./sl2.sv"
 
-module tb_sl5
+module tb_sl2
   // ---- Defining variables to be used ---- //
 	#(parameter n = 32);
 	reg [(n-1):0] num;   //inputs are reg for test bench
-	wire [(n-1):0] num32;     //outputs are wire for test bench
+	wire [(n-1):0] num4;     //outputs are wire for test bench
    
 	// ---- INSTANTIATE UNIT UNDER TEST (UUT) ---- //
-	sl5 uut(.num(num), .num32(num32));
+	sl2 uut(.num(num), .num4(num4));
 	
 	// ---- INITIALIZE TEST BENCH ---- //
 	initial begin
-		$dumpfile("sl5.vcd"); // for Makefile, make dump file same as module name
+		$dumpfile("sl2.vcd"); // for Makefile, make dump file same as module name
 		$dumpvars(0, uut);
-		$monitor("num=%b=%d, num32=%b=%d", num, num, num32, num32);
+		$monitor("num=%b=%d, num4=%b=%d", num, num, num4, num4);
 	end
 
 	//apply input vectors
 	initial begin: apply_stimulus
 		#1 num = 32'h00000000;
 		#1 num = 32'h00000001;
-		#1 num = 32'h00000111;
-		#1 num = 32'h0000BEEF;
 		#1 num = 32'h11111111;
 		#1 num = 32'h13579BDF;
 		#1 num = 32'hF0000000;
