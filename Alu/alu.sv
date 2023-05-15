9da6ba793a2445e9aaa3512310a5c684a31f2e6d
Ridwan Hussain <ridwanhussain2003@gmail.com>
2023-05-14 20:12:29 -0400

    Fixed Alu.sv file

--- Alu/alu.sv
+++ Alu/alu.sv
@@ -20,7 +20,7 @@ module alu(clk, a, b, alucontrol, result, pc, zero);
     
     input  logic clk;
     input  logic [31:0] a, b;
-    input  logic [2:0]  alucontrol;
+    input  logic [3:0]  alucontrol;
     output logic [31:0] result, pc;
     output logic zero;
 
@@ -43,41 +43,41 @@ module alu(clk, a, b, alucontrol, result, pc, zero);
         case (alucontrol)
             	4'b0000: result = a + b; // add
             	4'b0001: result = a + (~b + 1'b1); // sub
-		4'b0100: result = a << b; //sl
-		4'b0101: result = a >> b; //sr
-		4'b0110: result = a & b; // and
+							4'b0100: result = a << b; //sl
+							4'b0101: result = a >> b; //sr
+							4'b0110: result = a & b; // and
             	4'b0111: result = a | b; // or
-		4'b1000: result = a^b; // xor
-		4'b1001: result = ~(a | b); //nor
-		4'b1010: result = a; //jr
-		4'b1011: result = ~(a & b); // nand
-		4'b1100: result = ~(a); //not
+							4'b1000: result = a^b; // xor
+							4'b1001: result = ~(a | b); //nor
+							4'b1010: result = a; //jr
+							4'b1011: result = ~(a & b); // nand
+							4'b1100: result = ~(a); //not
 
-            4'b1101: begin                       // slt
-				if (a[31] != b[31])
-					if (a[31] > b[31])
-						result = 1;
-					else
-						result = 0;
-				else
-					if (a < b)
-						result = 1;
-					else
-						result = 0;
-            end
-	    4'1110: begin 
-	    			if (a[31] != b[31])
-					if (a[31] < b[31])
-						result = 1;
-					else
-						result = 0;
-				else
-					if (a > b)
-						result = 1;
-					else
-						result = 0;
-		end
-	endcase
+							4'b1101: begin                       // slt
+								if (a[31] != b[31])
+									if (a[31] > b[31])
+										result = 1;
+									else
+										result = 0;
+								else
+									if (a < b)
+										result = 1;
+									else
+										result = 0;
+							end
+							4'b1110: begin 
+								if (a[31] != b[31])
+									if (a[31] < b[31])
+										result = 1;
+									else
+										result = 0;
+								else
+									if (a > b)
+										result = 1;
+								else
+									result = 0;
+							end
+			endcase
     end
 
     //Multiply and divide results are only stored at clock falling edge.
