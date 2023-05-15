be12f1a42c618d243fb0dd96d331350c0e8780ac
Ridwan Hussain <ridwanhussain2003@gmail.com>
2023-05-15 00:37:42 -0400

    Changed control signals for mainDecoder, hopefully it works now.

--- MainDec/mainDec.sv
+++ MainDec/mainDec.sv
@@ -21,20 +21,25 @@ module mainDec(op, memtoreg, memwrite, branch, alusrc, regdst, regwrite, jump, a
 	output logic [1:0] aluop;
 
 	logic [8:0] controls;
-	assign {memtoreg, memwrite, branch, alusrc, regdst, regwrite, jump, aluop} = controls;
+	        //1       2         3
+	assign {memtoreg, memwrite, branch, 
+		//4      5       6
+		alusrc, regdst, regwrite, 
+		//7   8  9
+		jump, aluop} = controls;
 
 	always @* begin
 		// R type, set aluop to 
 		if ((op[4:3] == 2'b01) | (op[4:3] == 2'b10))
 		begin
-			controls = 9'b000011010;
+			controls = 9'b000011100;
 	    	end
 		case(op) //for alu decoder, 00 is add 11 is subtract
 			5'b11000: controls <= 9'b000101000; // ADDI 
 			5'b11001: controls <= 9'b000101011; // SUBI
 			5'b11010: controls <= 9'b100101000; // LW
 			5'b11011: controls <= 9'b010100000; // SW
-			5'b11100: controls <= 9'b001000011; // BEQ
+			5'b11100: controls <= 9'b001000001; // BEQ
 			5'b11101: controls <= 9'b000000011; // BNE
 			5'b00000: controls <= 9'b000000100; // J
 			5'b00111: controls <= 9'b000000100; // JAL
